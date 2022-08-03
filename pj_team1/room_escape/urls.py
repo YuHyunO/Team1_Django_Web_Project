2ddf4f34d33459d1b7198acff1486555ff36a025
R0hGR <cd1063@gmail.com>
2022-08-03 18:06:29 +0900

    updated

--- pj_team1/room_escape/urls.py
+++ pj_team1/room_escape/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),   
     path('cafe/', views.cafe, name='cafe'),   
     path('theme/', views.theme, name='theme'),   
+    path('theme/theme_detail/', views.theme_detail, name='theme_detail'),  
+    path('login/', views.login, name='login'),    
+    path('signup/', views.signup, name='signup'),
 ]
--- pj_team1/room_escape/views.py
+++ pj_team1/room_escape/views.py
@@ -15,3 +15,15 @@ def theme(request):
     template = loader.get_template('theme.html')
     return HttpResponse(template.render({}, request))
 
+def theme_detail(request):
+    template = loader.get_template('theme_detail.html')
+    return HttpResponse(template.render({}, request))
+
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render({}, request))
+
+def signup(request):
+    template = loader.get_template('signup.html')
+    return HttpResponse(template.render({}, request))
+
