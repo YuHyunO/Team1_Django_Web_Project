baebff5d18f8824f621ef90d56f1c30be5e9028b
R0hGR <cd1063@gmail.com>
2022-08-04 17:21:38 +0900

    model updated

--- pj_team1/room_escape/models.py
+++ pj_team1/room_escape/models.py
@@ -1,16 +1,56 @@
 from django.db import models
+
 class Member(models.Model):
-    email = models.EmailField(max_length=50, primary_key=True) #시스템 자동생성 되는 id컬럼(pk)이 생성되지 않음
-    pw = models.TextField(max_length=30)
-    name = models.CharField(max_length=30) #이름 혹은 별명
-    phone = models.TextField(max_length=50)
-    rdate = models.DateTimeField() #회원가입일(최초)
-    udate = models.DateTimeField() #회원정보 수정일
+    email = models.EmailField(primary_key=True)
+    pwd = models.TextField()
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=50)
+    rdate = models.DateTimeField()
+    udate = models.DateTimeField()
     
-class Board(models.Model): #primary key 는 id 컬럼
-    email = models.EmailField(max_length=50) # CharField 보다 더 많이 적을 수 있음
-    name = models.CharField(max_length=30) #이름 혹은 별명
-    title = models.TextField(max_length=100)
+class Board(models.Model):
+    no = models.AutoField(primary_key=True)
+    email = models.ForeignKey(Member, related_name='Board_email', on_delete=models.CASCADE, db_column='email')
+    # 테이블에 이름을 넣을지 아니면 view에서 template으로 넘어갈때
+    # #Member.name 을 같이 넘겨줄 지 고민.
+    type = models.CharField(max_length=20)
+    title = models.TextField(max_length=1000)
     content = models.TextField()
-    rdate = models.DateTimeField() #게시글 등록일
-    udate = models.DateTimeField() #게시글 수정일
+    rdate = models.DateTimeField()
+    udate = models.DateTimeField()
+    
+class Room(models.Model):
+    room = models.TextField(primary_key=True)
+    loc = models.TextField()
+    url = models.CharField(max_length=255)
+    tel = models.CharField(max_length=50)
+    theme_number = models.IntegerField(default=0)
+
+class CafeReview(models.Model):
+    no = models.AutoField(primary_key=True)
+    email = models.ForeignKey(Member, related_name='CafeReview_email', on_delete=models.CASCADE, db_column='email')
+    room = models.ForeignKey(Room, related_name='CafeReview_room', on_delete=models.CASCADE, db_column='room')
+    review = models.TextField()
+    rdate = models.DateTimeField()
+    udate = models.DateTimeField()
+    
+class Theme(models.Model):
+    theme = models.TextField(primary_key=True)
+    room = models.ForeignKey(Room, related_name='Theme_room', on_delete=models.CASCADE, db_column='room')
+    img_path = models.TextField()
+    genre = models.CharField(max_length=255)
+    people = models.CharField(max_length=10, default='-')
+    info = models.TextField(default='-')
+    difficulty = models.CharField(max_length=10, default='0')
+    horror = models.CharField(max_length=10, default='0')
+    activity = models.CharField(max_length=10, default='0')
+    star = models.CharField(max_length=10, default='0')
+    recommend = models.IntegerField(default=0)
+            
+class ThemeReview(models.Model):
+    no = models.AutoField(primary_key=True)
+    email = models.ForeignKey(Member, related_name='ThemeReview_email', on_delete=models.CASCADE, db_column='email')
+    theme = models.ForeignKey(Theme, related_name='ThemeReview_theme', on_delete=models.CASCADE, db_column='theme')
+    review = models.TextField()
+    rdate = models.DateTimeField()
+    udate = models.DateTimeField()
